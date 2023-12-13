d3344b9dafb9495e048519d96d82c6bbe5bf9b18
james599 <80703170+james599@users.noreply.github.com>
2023-12-13 21:23:04 +0000

    Update moderation.py

--- moderation.py
+++ moderation.py
@@ -1,6 +1,7 @@
 def check(query):
     from openai import OpenAI
-    client = OpenAI(api_key = "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")
+    import os
+    client = OpenAI(os.environ["OPENAI_KEY"])
 
     if client.moderations.create(input=query, model="text-moderation-latest").results[0].flagged == True:
         return "Please don't enter this type of content into this AI."
