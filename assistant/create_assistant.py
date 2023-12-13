20bcab8ac490a140a264be0b593678032b10de4c
james599 <80703170+james599@users.noreply.github.com>
2023-12-13 21:20:11 +0000

    Update create_assistant.py

--- assistant/create_assistant.py
+++ assistant/create_assistant.py
@@ -1,6 +1,7 @@
 def create(username, language, user_type):
     from openai import OpenAI
-    client = OpenAI(api_key = "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")
+    import os
+    client = OpenAI(os.environ["OPENAI_KEY"])
 
     if user_type is not None: # Teacher
         assistant = client.beta.assistants.create(
