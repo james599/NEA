bca9db16328b3612639fc866ac7bacdae859b612
james599 <80703170+james599@users.noreply.github.com>
2023-12-13 21:21:14 +0000

    Update display_thread.py

--- thread/display_thread.py
+++ thread/display_thread.py
@@ -1,7 +1,8 @@
 def display(thread_id):
 
     from openai import OpenAI
-    client = OpenAI(api_key = "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")
+    import os
+    client = OpenAI(os.environ["OPENAI_KEY"])
 
     messages = client.beta.threads.messages.list(
         thread_id=thread_id
