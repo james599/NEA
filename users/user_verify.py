04b635b666fbedce84d5c03b1b93c1d0603a7848
james599 <80703170+james599@users.noreply.github.com>
2024-01-29 12:48:42 +0000

    more secure hash

--- users/user_verify.py
+++ users/user_verify.py
@@ -1,10 +1,10 @@
 def verify(username, expected_password):
-    from hashlib import sha256
+    from hashlib import sha512
     user_db = open("users/users.txt", "r")
     db = user_db.readlines()
     user_db.close()
     for user_record in db:
-        expected_hash_password = sha256(expected_password.encode('utf-8') + user_record.split(",")[2].encode('utf-8')).hexdigest()
+        expected_hash_password = sha512(expected_password.encode('utf-8') + user_record.split(",")[2].encode('utf-8')).hexdigest()
         if user_record.split(",")[0] == username and user_record.split(",")[1] == expected_hash_password:
             return True
     return False
