7ac30f768c3328b0902fe17b6c2e1a181ad9b1d9
james599 <80703170+james599@users.noreply.github.com>
2024-03-26 15:36:36 +0000

    readability

--- thread/create_thread.py
+++ thread/create_thread.py
@@ -1,27 +1,31 @@
-def create(grade, file):
-    from openai import OpenAI
-    import os
+from openai import OpenAI
+import os
+def create(grade: str, file: any) -> str:
+    # Initialize the OpenAI API client
     client = OpenAI(os.environ["OPENAI_KEY"])
-
+    # Create a new thread
     thread = client.beta.threads.create()
-
+    # Create a new message with the specified content
     message = client.beta.threads.messages.create(
         thread_id=thread.id,
         role="user",
+        # Personalised context
         content=f"My target grade is a {grade}. Respond, in a short response. But only using the A-Level OCR curriculum. Your responses should be a sentence or two, unless the user’s request requires reasoning or long-form outputs. Only write the response, no pleasantries."
     )
     
     if file is not None:
         # TODO: Add regex on file
+        # Upload the file to the OpenAI API server
         uploaded_file = client.files.create(
             file=open(file, "rb"),
             purpose="assistants"
-        ) # adding file to openai servers
+        )
 
+        # Create a new connection between the assistant and the uploaded file/image
+        # To add file data to assistant model
         client.beta.assistants.files.create(
-            assistant_id=message.assistant_id, 
-            # retrieving assistant_id using json response from message 
+            assistant_id=message.assistant_id,  # id of assistant model
             file_id=uploaded_file.id
-        ) # adding file data to assistant model
+        )
 
     return thread.id
