7c84e71f643f9c0d1f62757575861943cf53c434
james599 <80703170+james599@users.noreply.github.com>
2024-03-26 15:17:56 +0000

    Readability

--- moderation.py
+++ moderation.py
@@ -1,8 +1,12 @@
 def check(query):
     from openai import OpenAI
     import os
+    
+    # Initialize the OpenAI client with the provided OpenAI API key
     client = OpenAI(os.environ["OPENAI_KEY"])
 
+    # Check if the input query is flagged by the OpenAI latest model
     if client.moderations.create(input=query, model="text-moderation-latest").results[0].flagged == True:
         return "Please don't enter this type of content into this AI."
+    # Return False if the input query passes moderation
     return False
