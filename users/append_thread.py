15fd10b67a9b8255364f806c87bf919b2cf98fc8
james599 <80703170+james599@users.noreply.github.com>
2024-03-26 15:20:43 +0000

    enumeration

--- users/append_thread.py
+++ users/append_thread.py
@@ -3,16 +3,18 @@ def append(username, thread):
     db = user_db.readlines()
     user_db.close()
 
-    line = -1
-    for user_record in db:
-        line += 1
+    for line_index, user_record in enumerate(db):
+        # If the username matches, replace the current line with the updated record
         if user_record.split(",")[0] == username:
             if "|" not in user_record:
-                db[line] = user_record.replace("\n","") + "|" + thread + "\n"
+                # If the user has no threads yet, add the thread to the record
+                db[line_index] = user_record.replace("\n", "") + "|" + thread + "\n"
             else:
-                threads = user_record.split('|')[1].replace("\n","")
-                db[line] = user_record.split('|')[0] + "|" + str(threads) + "|" + thread + "\n"
+                # If the user already has threads, append the new thread to the existing list
+                threads = user_record.split('|')[1].replace("\n", "")
+                db[line_index] = user_record.split('|')[0] + "|" + str(threads) + "|" + thread + "\n"
            
     with open('users/users.txt', 'w') as user_db:
         user_db.writelines(db)
+
     return 0
