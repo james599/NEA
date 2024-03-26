d537eb48ef15ebbfcb1ee6d35e1647d2ee786dd1
james599 <80703170+james599@users.noreply.github.com>
2024-03-26 15:38:10 +0000

    readability

--- thread/display_thread.py
+++ thread/display_thread.py
@@ -1,9 +1,9 @@
-def display(thread_id):
-
-    from openai import OpenAI
-    import os
+from openai import OpenAI
+import os
+def display(thread_id: str) -> array:
     client = OpenAI(os.environ["OPENAI_KEY"])
 
+    # Retrieve all messages associated with the specified thread
     messages = client.beta.threads.messages.list(
         thread_id=thread_id
     )
