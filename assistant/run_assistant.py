095e7a3d31c37bd7509e43726cfe97ec28b4a591
james599 <80703170+james599@users.noreply.github.com>
2024-03-26 15:45:08 +0000

    readability

--- assistant/run_assistant.py
+++ assistant/run_assistant.py
@@ -1,15 +1,16 @@
-def create(thread_id):
+from openai import OpenAI
+import os
+def create(thread_id: str) -> str:
     user_db = open("users/users.txt", "r")
     db = user_db.readlines()
     user_db.close()
     for user_record in db:
         if thread_id in user_record:
             assistant_id = user_record.split(",")[3].split("|")[0]
-
-    from openai import OpenAI
-    import os
+            
     client = OpenAI(os.environ["OPENAI_KEY"])
 
+    # Create run of a thread
     run = client.beta.threads.runs.create(
         thread_id=thread_id,
         assistant_id=assistant_id,
@@ -17,11 +18,10 @@ def create(thread_id):
 
     return run.id
 
-def status(thread_id, run_id):
-    from openai import OpenAI
-    import os
+def status(thread_id: str, run_id: str) -> bool:
     client = OpenAI(os.environ["OPENAI_KEY"])
 
+    # Checks status of run of a thread
     run = client.beta.threads.runs.retrieve(
         thread_id=thread_id,
         run_id=run_id
