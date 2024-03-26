968d2f0f4730b10c43d44eb4342dcc43c18f3aa7
james599 <80703170+james599@users.noreply.github.com>
2024-03-26 15:23:32 +0000

    readability

--- users/user_new.py
+++ users/user_new.py
@@ -1,8 +1,11 @@
-def append(username, password):
-    import string
-    import random
-    from hashlib import sha256
+import string
+import random
+from hashlib import sha256
+def append(username: str, password: str) -> int:
+    # Generate ansalt
     salt = ''.join(random.choices(string.ascii_lowercase + string.digits, k=64))
+    
+    # Hash the password using sha256 and the generated salt
     hash_password = sha256(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
     
     user_db = open("users/users.txt", "a")
@@ -10,11 +13,12 @@ def append(username, password):
     user_db.close()
     return 0
 
-def verify(username):
+def verify(username: str) -> bool:
     user_db = open("users/users.txt", "r")
     db = user_db.readlines()
     user_db.close()
     for user_record in db:
         if user_record.split(",")[0] == username:
             return True
+    # Return False if the username is not found
     return False
